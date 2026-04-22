50e71feb8b7ba705c2a053eb3be3483bc0b6a02f
Edson Rodrigues <er3303992@gmail.com>
2026-04-22 01:53:29 +0100

    refactor: reorganize event handlers for better readability and structure

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
+import { client, qrcode } from "./src/services/whatsapp.js";
+import { messageHandler } from "./src/handlers/messageHandler.js";
+import { logWithTimestamp } from "./src/utils/logger.js";
 
 // Compatibilidade com ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import { client, qrcode } from "./src/services/whatsapp.js";
-import { messageHandler } from "./src/handlers/messageHandler.js";
-import { logWithTimestamp } from "./src/utils/logger.js";
+logWithTimestamp("Iniciando a aplicação Dudu WhatsApp Bot...");
 
 // Eventos do cliente WhatsApp
+client.on("loading_screen", (percent, message) => {
+  logWithTimestamp(`Carregando... ${percent}% - ${message}`);
+});
+
+client.on("authenticated", () => {
+  logWithTimestamp("Cliente autenticado com sucesso!");
+});
+
+client.on("auth_failure", (msg) => {
+  logWithTimestamp(`Falha na autenticação: ${msg}`);
+});
+
 client.on("qr", (qr) => {
   logWithTimestamp("QR code gerado para autenticação.");
   qrcode.generate(qr, { small: true });
